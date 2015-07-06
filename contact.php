ee1ad8a950190def2e89503f6c4c33de1f2544b6
Matt Tarter <mjtarter@openmindwebs.com>
2015-07-06 12:42:51 -0600

    contact form updates

--- contact.php
+++ contact.php
@@ -9,51 +9,51 @@ include 'inc/header.php'; ?>
 		<div class="col-sm-9">
 			<p class="h2 text-center" style="margin-bottom:15px;">Get Started</p>
 			<p>Interested in my services? I would love to hear from you. Please fill out the following form to give me some information about your project and I will respond within two business days.</p>
-			<form name="contact-page-form" method="post" action="send_form_email.php">
-				<label for="example" style="display:block; text-align:Center;">Services Requested *</label>
-				 	<div id="example" class="text-center">
+			<form name="contact-page-form" method="post" action="get-started-confirmation.php">
+				<label for="services" style="display:block; text-align:center;">Services Requested *</label>
+				 	<div id="services" class="text-center">
 					<div class="checkbox" style="margin-top:0px;">
 					  	<label class="checkbox-inline">
-							<input type="checkbox" id="inlineCheckbox1" value="option1"> Web Development
+							<input type="checkbox" name="service_1" id="service_1" value="option1"> Web Development
 						</label>
 						<label class="checkbox-inline">
-						  	<input type="checkbox" id="inlineCheckbox2" value="option2"> Web Design
+						  	<input type="checkbox" name="service_2" id="service_2" value="option2"> Web Design
 						</label>
 						<label class="checkbox-inline">
-						  	<input type="checkbox" id="inlineCheckbox3" value="option3"> Database Management
+						  	<input type="checkbox" name="service_3" id="service_3" value="option3"> Database Management
 						</label>
 					</div>
 					<div class="checkbox">
 					  	<label class="checkbox-inline">
-						  	<input type="checkbox" id="inlineCheckbox1" value="option1"> Copywriting / SEO
+						  	<input type="checkbox" name="service_4" id="service_4" value="option1"> Copywriting / SEO
 						</label>
 						<label class="checkbox-inline">
-						  	<input type="checkbox" id="inlineCheckbox2" value="option2"> E-commerce
+						  	<input type="checkbox" name="service_5" id="service_5" value="option2"> E-commerce
 						</label>
 						<label class="checkbox-inline">
-						  	<input type="checkbox" id="inlineCheckbox3" value="option3"> Analytics
+						  	<input type="checkbox" name="service_6" id="service_6" value="option3"> Analytics
 						</label>
 					</div>
 				</div>
 				<div class="col-sm-6 form-group" style="padding-left:0px; clear:both;">
 	    			<label for="first_name">First Name *</label>
-	    			<input type="text" class="form-control" id="first_name" placeholder="First Name">
+	    			<input type="text" class="form-control" name="first_name" id="first_name" placeholder="First Name">
 	  			</div>
 	  			<div class="col-sm-6 form-group" style="padding-left:0px;">
 	    			<label for="last_name">Last Name *</label>
-	    			<input type="text" class="form-control" id="last_name" placeholder="Last Name">
+	    			<input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name">
 	  			</div>
 	  			<div class="col-sm-6 form-group" style="padding-left:0px;">
 	    			<label for="email">Email *</label>
-	    			<input type="email" class="form-control" id="email" placeholder="Email">
+	    			<input type="email" class="form-control" name="email" id="email" placeholder="Email">
 	  			</div>
 	  			<div class="col-sm-6 form-group" style="padding-left:0px;">
 	    			<label for="phone">Phone</label>
-	    			<input type="text" class="form-control" id="phone" placeholder="Phone">
+	    			<input type="text" class="form-control" name="phone" id="phone" placeholder="Phone">
 	  			</div>
 	  			<div class="col-sm-6 form-group" style="padding-left:0px;">
-				 	<label for="sel1">Budget</label>
-				  	<select class="form-control" id="sel1">
+				 	<label for="budget">Budget</label>
+				  	<select class="form-control" name="budget" id="budget">
 				    	<option>>$1,000</option>
 				  		<option>$1,000 - $ 3,000</option>
 				  		<option>$3,000 - $5,000</option>
@@ -63,14 +63,25 @@ include 'inc/header.php'; ?>
 				</div>
 				<div class="col-sm-6 form-group" style="padding-left:0px;">
 	    			<label for="business_category">Business Category</label>
-	    			<input type="text" class="form-control" id="business_category" placeholder="e.g. restaurant, photography">
+	    			<input type="text" class="form-control" name="business_category" id="business_category" placeholder="e.g. restaurant, photography">
 	  			</div>
 				<div class="col-sm-12 form-group" style="padding-left:0px;">
-  					<label for="comment">Project Summary:</label>
-  					<textarea class="form-control" rows="5" id="comment"></textarea>
+  					<label for="summary">Project Summary:</label>
+  					<textarea class="form-control" rows="5" name="summary" id="summary"></textarea>
 				</div>
-				<button class="button orange-button center-block m-vert-20" style="display:block;">Submit</button>
+				<input type="submit" class="button orange-button center-block m-vert-20" style="display:block;" value="Submit">
 			</form>
+		</div>
+		<div class="col-sm-3 text-center">
+			<p class="h2 hidden-xs">Contact</p>
+			<p>Have a question or need to contact me for purposed other than web services?</p>
+			<button class="button orange-button center-block m-vert-20" style="display:block;">Click Here</button>
+			 <ul class="hidden-xs">
+               <li><p><i class="fa fa-github-square"></i> <a href="https://github.com/mjtarter" target="_blank">Github</a></p></li>
+               <li><p><i class="fa fa-linkedin-square"></i> <a href="https://www.linkedin.com/in/mjtarter" target="_blank">Linked In</a></p></li>
+               <li><p><span class="glyphicon glyphicon-user"></span> About Me</p></li>
+               <li><p><span class="glyphicon glyphicon-briefcase"></span> Portfolio</p></li>
+            </ul>
 		</div>
 	</div>
 </section>
