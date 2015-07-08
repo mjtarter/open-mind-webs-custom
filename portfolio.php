7a7a11097665ec0d76458e590bbcda2615cc42aa
Matt Tarter <mjtarter@openmindwebs.com>
2015-07-08 15:39:47 -0600

    added all projects to portfolio page

--- portfolio.php
+++ portfolio.php
@@ -4,4 +4,107 @@ $title = "Portfolio | Web Design | Marketing";
 
 include 'inc/header.php'; ?>
 
+<div class="bg">
+	<div class="jumbotron text-center" id="portfolio-jumbo">
+		<div class="container">
+			<p class="h1"><b>View My Latest Work</b></p>
+			<hr>
+			<p class="hidden-xs">Big or small projects. Responsive, static, simple, or complex websites.</p>
+			<p class="hidden-xs">You name it, I can help.</p>
+			<a href="#websites"><button class="button orange-button">Websites</button></a>
+			<a href="#dbms"><button class="button orange-button">DBMS</button></a>
+			<a href="#copywriting"><button class="button orange-button">Copywriting</button></a>
+		</div>
+	</div>
+	<section class="tan-bg">
+		<div class="container">
+			<div class="row">
+				<p class="text-center h1 portfolio-titles" id="websites">Responsive Websites</p>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/mci-responsive.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">MyCampus Island</a></p>
+					<p>MyCampus Island (my startup) is a web based student housing app for college students in the Bloomington, Indiana area. This is a large site that takes advantage of an extensive back-end database to provide... <a href="#">&raquo;More</a></p>
+				</div>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/dropping-dimes-responsive.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">Dropping Dimes Foundation</a></p>
+					<p>Dropping Dimes is a charitable organization with an aim to assist former ABA basketball players living in economically or medically disadvantaged situations. They came to me when their organization was... <a href="#">&raquo;More</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="orange-bg" style="border-bottom:7px solid tan">
+		<div class="container">
+			<div class="row">
+				<p class="text-center h1 portfolio-titles">More Websites</p>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/abrams-eyecare-website.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">Abrams Eyecare</a></p>
+					<p>One of the top ophthalmology practices in Indiana, Abrams Eyecare has 3 offices located in central Indiana. They had an outdated website that had not been changed in years, so I redesigned and developed... <a href="#">&raquo;More</a></p>
+				</div>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/ted-green-films-website.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">Ted Green Films</a></p>
+					<p>An Indianapolis based film producer, Ted Green Films has been awarded 8 Regional Emmys for their documentaries. The website I created provides online marketing for their documentaries, recent news, previews,... <a href="#">&raquo;More</a></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<a href="#"><img src="img/rb-website.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">R&amp;B Architects</a></p>
+					<p>Located in Indianapolis, IN, R&amp;B Architects had been in existence for quite awhile but still was in need of a website. I designed and developed their brand new site... <a href="#">&raquo;More</a></p>
+				</div>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/tarter-bros-website.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">Tarter Bros Staining</a></p>
+					<p>A few years ago when my brother and I owned and operated a deck staining business, I developed this simple site to get the word out and help potential customers to contact us... <a href="#">&raquo;More</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="tan-bg">
+		<div class="container">
+			<div class="row">
+				<p class="text-center h1 portfolio-titles" id="dbms">Database Management</p>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/the-only-dbms.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">The Only</a></p>
+					<p>The Only is a sports exhibition hall that consists of thousands of historical sports pieces and memorabilia. For this project the client needed an organized database to display all of their inventory... <a href="#">&raquo;More</a></p>
+				</div>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/hoffmaster-dbms.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">Hoffmaster</a></p>
+					<p>Hoffmaster is a market leading North American company specializing in the fields of Foodservice, Consumer, and Contract Manufacturing. They came to us with the need for a DBMS to assist them with organizing... <a href="#">&raquo;More</a></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6">
+					<a href="#"><img src="img/collection-excel.png" class="img-responsive center-block"></a>
+					<p class="h3 text-center"><a href="#">Client Personal Collection</a></p>
+					<p>This client had an ENORMOUS collection of sports memorabilia and needed a cost efficient way to organize it to know everything that he had as well as to have paperwork for insurance purposes... <a href="#">&raquo;More</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="orange-bg">
+		<div class="container">
+			<div class="row">
+				<p class="text-center h1 portfolio-titles" id="copywriting">Copywriting</p>
+				<div class="col-sm-6">
+					<a href="#"><img src="img/a.png" class="img-responsive center-block"></a>
+				</div>
+				<div class="hidden-xs col-sm-6">
+					<a href="#"><img src="img/b.png" class="img-responsive center-block"></a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<p class="h3 text-center">Lodge Design</p>
+					<p>Since the spring of 2014 I have been contracted by <a href="http://www.lodgedesign.com">Lodge Design</a> to assist with uploading products to target.com, walmart.com, and staples.com. I have worked on the back end of each of these enormous e-commerce systems to upload thousands of new products. I also work on the front end writing product descriptions for SEO and developing consumer facing content.</a>... <a href="#">&raquo;More</a></p>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
 <?php include 'inc/footer.php'; ?>  
