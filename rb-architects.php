c520cbb6c085be7449bf5cd2ee3e0f2a0a54fc08
Matt Tarter <mjtarter@openmindwebs.com>
2016-08-06 12:03:07 -0600

    Delete rb architects portfolio

--- rb-architects.php
+++ rb-architects.php
@@ -1,14 +1,12 @@
-<?php 
+<?php
 
 $title = "R&amp;B Architects | Website | Portfolio";
 $name = "R&amp;B Architects";
-$link = "rb-architects";
-$url = "randbarchitects.com";
 $services = array("Web Development", "Web Design");
 $description = "Located in Indianapolis, IN, R&B Architects had been in existence for quite awhile but still was in need of a website. They weren’t sure what direction they wanted to take with the website so I designed multiple versions and developed their brand new site.";
 $img_src = "img/rb-website.png";
 $img2_src = "img/randb-page.jpg";
 
-include 'inc/individual_portfolio_temp.php'; 
+include 'inc/individual_portfolio_temp.php';
 
-?>  
+?>
